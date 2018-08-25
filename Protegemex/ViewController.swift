bb898b93c65ffe400905ff6f93704b1ae0e15f58
Samuel Castillo <Samuel@samuel-castillo.local>
2018-08-25 15:47:52 -0500

    Initial Commit

---
+++ Protegemex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Protegemex
+//
+//  Created by Samuel Castillo on 8/25/18.
+//  Copyright © 2018 hackaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
